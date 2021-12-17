2933249a1537d18838d3dd752af0a4238c621d3c
MikevPeeren <mikevpeeren@hotmail.com>
2021-12-17 13:21:18 +0100

    :sparkles: added animation for menu to appear

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 
 import Image from "next/image";
 
+// External
+import classNames from "classnames";
+
 import LOGO from "../public/shared/logo.svg";
 import HAMBURGER from "../public/shared/icon-hamburger.svg";
 import CLOSE from "../public/shared/icon-close.svg";
@@ -11,43 +14,52 @@ import styles from "./Header.module.scss";
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
+  // Initializing menu
+  const navBarClassNames = classNames(
+    `${styles.hamburgerMenu} fixed w-3/5 h-full right-0 z-2 ease-in-out duration-1000 transform`,
+    {
+      "translate-x-0": isMenuOpen,
+      "translate-x-full": !isMenuOpen,
+    }
+  );
+
   return (
     <header className="relative">
       <div className="fixed w-full z-0">
         <div className="flex flex-row items-center justify-between p-4">
           <Image src={LOGO} alt="" width={40} height={40} />
-          <Image
-            src={HAMBURGER}
-            alt=""
-            width={24}
-            height={21}
-            onClick={() => setIsMenuOpen(true)}
-          />
+          <button onClick={() => setIsMenuOpen(true)}>
+            <Image src={HAMBURGER} alt="" width={24} height={21} />
+          </button>
         </div>
       </div>
-      {isMenuOpen && (
-        <aside
-          className={`${styles.hamburgerMenu} fixed w-3/5 h-full right-0 z-2 bg-pink-300`}
-        >
-          <button className="flex flex-row ml-auto justify-end p-4">
+
+      <aside className={navBarClassNames}>
+        <div className="flex flex-row items-center justify-between p-4">
+          <button
+            className="flex flex-row ml-auto items-center justify-center w-10 h-10"
+            onClick={() => {
+              setIsMenuOpen(false);
+            }}
+          >
             <Image src={CLOSE} alt="" width={20} height={21} />
           </button>
-          <ul className="mt-20 ml-8 w-full">
-            <li className="my-10 uppercase">
-              <span className="pr-2 font-bold">00</span> Home
-            </li>
-            <li className="my-10 uppercase">
-              <span className="pr-2.5 font-bold">01</span> Destination
-            </li>
-            <li className="my-10 uppercase">
-              <span className="pr-2 font-bold">02</span> Crew
-            </li>
-            <li className="my-10 uppercase">
-              <span className="pr-2 font-bold">03</span> Technology
-            </li>
-          </ul>
-        </aside>
-      )}
+        </div>
+        <ul className="mt-20 ml-8 w-full">
+          <li className="my-10 uppercase">
+            <span className="pr-2 font-bold">00</span> Home
+          </li>
+          <li className="my-10 uppercase">
+            <span className="pr-2.5 font-bold">01</span> Destination
+          </li>
+          <li className="my-10 uppercase">
+            <span className="pr-2 font-bold">02</span> Crew
+          </li>
+          <li className="my-10 uppercase">
+            <span className="pr-2 font-bold">03</span> Technology
+          </li>
+        </ul>
+      </aside>
     </header>
   );
 };
