d539f38995fbbb89a71f6a35b00b877f8b2c8d3a
MikevPeeren <mikevpeeren@hotmail.com>
2021-12-18 16:21:57 +0100

    :sparkles: added explore button

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,20 +10,24 @@ const Home = () => {
         <meta name="description" content="Created by Mike van Peeren" />
         <link rel="icon" href="/favicon.svg" />
       </Head>
-      <main className="flex flex-col min-h-screen text-white text-center">
+      <main className="flex flex-col min-h-screen text-center">
         <div className="h-20">
           <Header />
         </div>
 
-        <div className="flex flex-col justify-start items-center min-h-screen text-white text-center my-10 mx-1">
+        <div className="flex flex-col justify-start items-center min-h-screen text-center mt-10 mx-1">
           <h1 className="uppercase my-6">So, you want to travel to</h1>
-          <h2 className="uppercase">Space</h2>
+          <h2 className="uppercase text-white">Space</h2>
           <h3 className="my-6">
             Let’s face it; if you want to go to space, you might as well
             genuinely go to outer space and not hover kind of on the edge of it.
             Well sit back, and relax because we’ll give you a truly out of this
             world experience!
           </h3>
+
+          <button className="bg-white mt-40 border-1 rounded-full w-[150px] h-[150px] font-bellefair text-xl uppercase tracking-wider leading-6">
+            Explore
+          </button>
         </div>
       </main>
       <footer></footer>
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,10 @@ module.exports = {
         blue: "#D0D6F9",
         white: "#FFFFFF",
       },
+      fontFamily: {
+        bellefair: ["Bellefair", "serif"],
+        barlow: ["Barlow Condensed", "sans serif"],
+      },
     },
   },
   variants: {
