2f9b709e8ae5573e00981f0a496650ccde04e4db
MikevPeeren <mikevpeeren@hotmail.com>
2022-01-08 14:41:19 +0100

    :sparkles: added active states for menu

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 // External
 import classNames from "classnames";
@@ -15,7 +16,9 @@ import styles from "./Header.module.scss";
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  // Initializing menu
+  const router = useRouter();
+
+  // Initializing menu classes
   const navBarClassNames = classNames(
     `${styles.hamburgerMenu} fixed top-0 w-3/5 min-h-screen right-0 z-2 text-left ease-in-out duration-1000 transform`,
     {
@@ -49,28 +52,50 @@ const Header = () => {
         <ul className="mt-20 ml-8 w-full">
           <li className="my-10 uppercase">
             <Link href="/" passHref>
-              <a className="font-bold">
+              <a
+                className={`font-bold ${
+                  router.pathname === "/" ? "border-l-4 border-white pl-2" : ""
+                }`}
+              >
                 00 <span className="pl-2">Home</span>
               </a>
             </Link>
           </li>
           <li className="my-10 uppercase">
             <Link href="/destination" passHref>
-              <a className="font-bold">
-                00 <span className="pl-2"> Destination</span>
+              <a
+                className={`font-bold ${
+                  router.pathname === "/destination"
+                    ? "border-l-4 border-white pl-2"
+                    : ""
+                }`}
+              >
+                01 <span className="pl-2"> Destination</span>
               </a>
             </Link>
           </li>
           <li className="my-10 uppercase">
             <Link href="/crew" passHref>
-              <a className="font-bold">
-                00 <span className="pl-2"> Crew</span>
+              <a
+                className={`font-bold ${
+                  router.pathname === "/crew"
+                    ? "border-l-4 border-white pl-2"
+                    : ""
+                }`}
+              >
+                02 <span className="pl-2"> Crew</span>
               </a>
             </Link>
           </li>
           <li className="my-10 uppercase">
             <Link href="/technology" passHref>
-              <a className="font-bold">
+              <a
+                className={`font-bold ${
+                  router.pathname === "/technology"
+                    ? "border-l-4 border-white pl-2"
+                    : ""
+                }`}
+              >
                 00 <span className="pl-2"> Technology</span>
               </a>
             </Link>
