1bde042a8853544dd44ee68de4f4432db1bb8c28
MikevPeeren <mikevpeeren@hotmail.com>
2022-01-14 22:08:56 +0100

    :sparkles: added onClick change for crew members

--- pages/crew.tsx
+++ pages/crew.tsx
@@ -1,12 +1,24 @@
+import React, { useState } from "react";
+
 import Head from "next/head";
+import Image from "next/image";
+
+import clsx from "clsx";
 
 import Header from "../components/Header";
 
-import Image from "next/image";
-
 import DOUGLAS from "../public/crew/image-douglas-hurley.webp";
+import MARK from "../public/crew/image-mark-shuttleworth.webp";
+import VICTOR from "../public/crew/image-victor-glover.webp";
+import ANOUSHEH from "../public/crew/image-anousheh-ansari.webp";
 
 const Crew = () => {
+  const [activeButton, setActiveButton] = useState(1);
+
+  const handleCrewChange = (event) => {
+    setActiveButton(event.target.id);
+  };
+
   return (
     <div className="background--crew">
       <Head>
@@ -24,17 +36,62 @@ const Crew = () => {
           <div className="flex flex-col justify-between items-center mt-8 border-b border-dark-gray w-5/6">
             <div className="w-2/4">
               <Image
-                src={DOUGLAS}
+                src={
+                  activeButton == 1
+                    ? DOUGLAS
+                    : activeButton == 2
+                    ? MARK
+                    : activeButton == 3
+                    ? VICTOR
+                    : ANOUSHEH
+                }
                 alt="Picture of the author"
                 layout="responsive"
+                priority
               />
             </div>
           </div>
           <div className="flex flex-row justify-between items-center mt-8">
-            <button className="rounded-full bg-white w-3 h-3 mx-2"></button>
-            <button className="rounded-full bg-white w-3 h-3 mx-2 bg-opacity-20"></button>
-            <button className="rounded-full bg-white w-3 h-3 mx-2 bg-opacity-20"></button>
-            <button className="rounded-full bg-white w-3 h-3 mx-2 bg-opacity-20"></button>
+            <button
+              id="1"
+              className={clsx({
+                "rounded-full bg-white w-3 h-3 mx-2 cursor-pointer": true,
+                "bg-opacity-20": activeButton != 1,
+              })}
+              onClick={(event) => {
+                handleCrewChange(event);
+              }}
+            ></button>
+            <button
+              id="2"
+              className={clsx({
+                "rounded-full bg-white w-3 h-3 mx-2 cursor-pointer": true,
+                "bg-opacity-20": activeButton != 2,
+              })}
+              onClick={(event) => {
+                handleCrewChange(event);
+              }}
+            ></button>
+            <button
+              id="3"
+              className={clsx({
+                "rounded-full bg-white w-3 h-3 mx-2 cursor-pointer": true,
+                "bg-opacity-20": activeButton != 3,
+              })}
+              onClick={(event) => {
+                handleCrewChange(event);
+              }}
+            ></button>
+            <button
+              id="4"
+              className={clsx({
+                "rounded-full bg-white w-3 h-3 mx-2 cursor-pointer": true,
+                "bg-opacity-20": activeButton != 4,
+              })}
+              onClick={(event) => {
+                handleCrewChange(event);
+              }}
+            ></button>
           </div>
           <div className="flex flex-col justify-between items-center mt-8">
             <h2 className="uppercase text-base text-white opacity-50">
