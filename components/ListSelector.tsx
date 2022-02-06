946dc16f86d3243795985e2b38a2325ff565880c
MikevPeeren <mikevpeeren@hotmail.com>
2022-02-06 17:10:18 +0100

    :sparkles: start on destination page

--- components/ListSelector.tsx
+++ components/ListSelector.tsx
@@ -11,10 +11,13 @@ const ListSelector = ({ activeButton, setActiveButton, content, variant }) => {
             "cursor-pointer transition ease-in-out delay-150 duration-200":
               true,
             "rounded-full bg-white w-3 h-3 mx-2": variant === "sm",
+            "bg-opacity-20": activeButton != i,
             "rounded-full bg-black text-white border border-dark-gray w-10 h-10 mx-2":
               variant === "md",
-            "bg-opacity-20": activeButton != i,
             "!bg-white text-black": activeButton === i && variant === "md",
+            "text-blue tracking-widest uppercase w-10 h-10 mx-4":
+              variant === "underlined",
+            "text-white": activeButton === i && variant === "underlined",
           })}
           onClick={() => setActiveButton(i)}
         >
--- pages/destination/index.tsx
+++ pages/destination/index.tsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
+
 import Head from "next/head";
+import Image from "next/image";
 
 import Header from "../../components/Header";
 
+import MOON from "/public/destination/image-moon.webp";
+import MARS from "/public/destination/image-mars.webp";
+import EUROPA from "/public/destination/image-europa.webp";
+import TITAN from "/public/destination/image-titan.png";
+import ListSelector from "@/components/ListSelector";
+
 const Destination = () => {
+  const [activeButton, setActiveButton] = useState(0);
+
   return (
-    <div className="background">
+    <div className="background--destination">
       <Head>
         <title>Space Destination</title>
       </Head>
@@ -13,7 +24,55 @@ const Destination = () => {
           <Header />
         </div>
 
-        <div className="flex flex-col justify-start items-center min-h-screen text-center mt-10 mx-1"></div>
+        <div className="flex flex-col justify-start items-center text-center mt-4 mx-1 ">
+          <h1 className="uppercase my-6 text-base text-white tracking-widest">
+            <span className="opacity-25 pr-4 font-bold">01</span> Pick your
+            destination
+          </h1>
+          <div className="flex flex-col justify-between items-center mt-8 w-5/6">
+            <div className="w-3/5">
+              {activeButton === 0 && (
+                <Image
+                  src={MOON}
+                  alt="Picture of the planet Moon"
+                  layout="responsive"
+                  priority
+                />
+              )}
+              {activeButton === 1 && (
+                <Image
+                  src={MARS}
+                  alt="Picture of the planet Mars"
+                  layout="responsive"
+                  priority
+                />
+              )}
+              {activeButton === 2 && (
+                <Image
+                  src={EUROPA}
+                  alt="Picture of the planet Europa"
+                  layout="responsive"
+                  priority
+                />
+              )}
+              {activeButton === 3 && (
+                <Image
+                  src={TITAN}
+                  alt="Picture of the planet Titan"
+                  layout="responsive"
+                  priority
+                />
+              )}
+            </div>
+          </div>
+
+          <ListSelector
+            activeButton={activeButton}
+            setActiveButton={setActiveButton}
+            content={["Moon", "Mars", "Europa", "Titan"]}
+            variant="underlined"
+          />
+        </div>
       </main>
       <footer></footer>
     </div>
