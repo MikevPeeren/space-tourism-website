8d80d2dfd52dfd8bd36efeb1cd362d922f0bdf41
MikevPeeren <mikevpeeren@hotmail.com>
2022-03-17 20:28:48 +0100

    :dizzy: added hover effects

--- components/ListSelector.tsx
+++ components/ListSelector.tsx
@@ -31,12 +31,14 @@ const ListSelector = ({
           className={classNames({
             "cursor-pointer transition ease-in-out delay-150 duration-200 my-4":
               true,
-            "rounded-full bg-white w-3 h-3 mx-2": variant === "sm",
+            "rounded-full bg-white w-3 h-3 mx-2 hover:bg-gray":
+              variant === "sm",
+            "!bg-white": activeButton === i && variant === "sm",
             "bg-opacity-20": activeButton != i,
-            "rounded-full bg-black text-white border border-dark-gray lg:w-16 lg:h-16 w-10 h-10 mx-2":
+            "rounded-full bg-black text-white border border-dark-gray lg:w-16 lg:h-16 w-10 h-10 mx-2 hover:outline-white hover:outline-1 hover:outline":
               variant === "md",
             "!bg-white text-black": activeButton === i && variant === "md",
-            "text-blue tracking-widest uppercase w-10 h-10 mx-4":
+            "text-blue tracking-widest uppercase w-10 h-10 mx-4 hover:underline hover:underline-offset-8":
               variant === "underlined",
             "text-white underline decoration-2 underline-offset-8":
               activeButton === i && variant === "underlined",
